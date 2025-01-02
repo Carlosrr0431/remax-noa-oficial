35e7a340e9fa8793838212a34541a1857a49043a
Carlos RR <carlos.facundo.rr@gmail.com>
2025-01-02 09:47:24 -0300

    asd

--- src/app/(components)/landingInmobilaria/plataforma-reclutamiento/segundaEntrevista/randomLinks.js
+++ src/app/(components)/landingInmobilaria/plataforma-reclutamiento/segundaEntrevista/randomLinks.js
@@ -1,19 +1,19 @@
-function hash(str) {
-  let hash = 0;
-  for (let i = 0; i < str.length; i++) {
-    const char = str.charCodeAt(i);
-    hash = (hash << 5) - hash + char;
-    hash = hash & hash; // Convert to 32bit integer
-  }
-  return hash;
-}
+// function hash(str) {
+//   let hash = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     const char = str.charCodeAt(i);
+//     hash = (hash << 5) - hash + char;
+//     hash = hash & hash; 
+//   }
+//   return hash;
+// }
 
-export function generateUniqueId() {
-  const timestamp = Date.now();
-  const random = Math.random();
+// export function generateUniqueId() {
+//   const timestamp = Date.now();
+//   const random = Math.random();
 
-  const combinedString = `${timestamp}-${random}`;
-  const hashedValue = hash(combinedString);
+//   const combinedString = `${timestamp}-${random}`;
+//   const hashedValue = hash(combinedString);
 
-  return hashedValue.toString(36); // Convert to base36 for shorter string
-}
+//   return hashedValue.toString(36); 
+// }
